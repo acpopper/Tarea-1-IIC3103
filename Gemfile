32dfff8723a8534a0037863afd8c9ed1744c5895
acpopper <acpopper@uc.cl>
2021-04-11 01:51:35 -0400

    Primeros pasos con la API

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '3.0.0'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
+# HTTP
+gem 'http', '~> 4.4', '>= 4.4.1'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,15 @@
+require "http"
+require 'json'
+
 class WelcomeController < ApplicationController
+    
+    def index
+        response = HTTP.get("https://tarea-1-breaking-bad.herokuapp.com/api/characters")
+        results = JSON.parse(response.to_str)
+        name = results[0]['name']
+        @name = name;
+
+
+    end
+
 end
