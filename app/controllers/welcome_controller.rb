5a0790eb7f915a51eb0a2bc0fb8879ff1d8b0119
acpopper <acpopper@uc.cl>
2021-04-11 02:19:54 -0400

    agregadas algunas cosas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,13 +3,23 @@ require 'json'
 
 class WelcomeController < ApplicationController
     
-    def index
+    def series
         response = HTTP.get("https://tarea-1-breaking-bad.herokuapp.com/api/characters")
         results = JSON.parse(response.to_str)
         name = results[0]['name']
         @name = name;
+    end
 
+    def episodios
+    end
 
+    def detalle_episodio
+    end
+
+    def detalle_personaje
+    end
+
+    def personajes
     end
 
 end
